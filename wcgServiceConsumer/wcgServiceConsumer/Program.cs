0e16fc877d96efb8ae31d54be0e6a36e7f5a120c
PeterKruhoffer <Peter.kruhoffer@gmail.com>
2018-02-12 13:44:30 +0100

    opgave b startet

---
+++ wcgServiceConsumer/wcgServiceConsumer/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using wcgServiceConsumer.ServiceReference1;
+
+namespace wcgServiceConsumer
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student();
+
+            using (var client = new ServiceReference1.Service1Client())
+            {
+                Console.WriteLine(client.AddStudent(s1));
+            }
+            Console.ReadLine();
+
+            
+        }
+    }
+}
